8be3d7abcaf22e2b53126577bd6a19dbf935dbe9
zhengxianyu <zhengxianyu@gjzq.com.cn>
2020-11-17 16:31:23 +0800

    add codes of unique excel

--- index.js
+++ index.js
@@ -321,6 +321,56 @@ $(function() {
     fileReader.readAsBinaryString(files[0]);
   });
 
+
+  // 给excel去重
+  $('#uniqueData').change(function(parentEvent) {
+    let files = parentEvent.target.files;
+
+    let fileReader = new FileReader();
+    let getExcelList = [];
+    fileReader.onload = function(childEvent) {
+      let excelBinaryData;
+      // 读取上传的excel文件
+      try {
+        let excelData = childEvent.target.result;
+        excelBinaryData = XLSX.read(excelData, {
+          type: 'binary'
+        });
+      } catch (parentEvent) {
+        console.log('该文件类型不能识别');
+        return;
+      }
+
+      // 获取excel所有元素
+      for (let sheet in excelBinaryData.Sheets) {
+        if (excelBinaryData.Sheets.hasOwnProperty(sheet)) {
+          let excelSheet = XLSX.utils.sheet_to_json(excelBinaryData.Sheets[sheet]);
+          getExcelList[getExcelList.length] = excelSheet;
+        }
+      }
+
+      uniqueData = handleUniqueData(getExcelList[0])
+      
+      downloadOneSheet(uniqueData, files);
+    };
+
+    // 以二进制方式打开文件
+    fileReader.readAsBinaryString(files[0]);
+  });
+  // 给excel去重
+
+  function handleUniqueData(arr) {
+    for (var index = 0; index < arr.length; index++) {
+      for (var i = index + 1; i < arr.length; i++) {
+        if (arr[i]['代码'] == arr[index]['代码']) {
+          arr.splice(i, 1)
+          i--
+        }
+      }
+    }
+    return arr
+  }
+
   function getAllCourse(keyName, oldItem) {
     let item = {};
     item['1、您的姓名：'] = oldItem['1、您的姓名：'];
